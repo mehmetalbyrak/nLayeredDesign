60f440c9a30ed4aeb52a19b3ee5c0c40d0208e4a
mehmetalbyrak <albayrakmehmetdeu@gmail.com>
2024-10-29 20:44:25 +0300

    Add project files.

---
+++ nLayeredDesign.Entity/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nLayeredDesign.Entity.Concrete
+{
+    public class Category
+    {
+        // field - variable - property 
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
---
+++ nLayeredDesign.Entity/Concrete/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nLayeredDesign.Entity.Concrete
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string  CustomerLastName { get; set; }
+        public string CustomerDistrict { get; set; }
+        public string CustomerCity { get; set; }
+    }
+
+    // solid --> medium article for Single Responsibility
+}
